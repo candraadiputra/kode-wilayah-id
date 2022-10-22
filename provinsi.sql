bb989f997a90c8abf97874e4252d33f340ce44b3
Candra <candraadiputra@gmail.com>
2022-10-22 17:10:14 +0700

    Add files via upload
    propinsi 34, Kabupaten 514, kecamatan 7160 , Desa 83184

---
+++ provinsi.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 22, 2022 at 11:47 AM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 5.6.37
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `provinsi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `provinsi`
+--
+
+CREATE TABLE `provinsi` (
+  `kode` varchar(13) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `provinsi`
+--
+
+INSERT INTO `provinsi` (`kode`, `nama`) VALUES
+('11', 'ACEH'),
+('12', 'SUMATERA UTARA'),
+('13', 'SUMATERA BARAT'),
+('14', 'RIAU'),
+('15', 'JAMBI'),
+('16', 'SUMATERA SELATAN'),
+('17', 'BENGKULU'),
+('18', 'LAMPUNG'),
+('19', 'KEPULAUAN BANGKA BELITUNG'),
+('21', 'KEPULAUAN RIAU'),
+('31', 'DKI JAKARTA'),
+('32', 'JAWA BARAT'),
+('33', 'JAWA TENGAH'),
+('34', 'DAERAH ISTIMEWA'),
+('35', 'JAWA TIMUR'),
+('36', 'BANTEN'),
+('51', 'BALI'),
+('52', 'NUSA TENGGARA BARAT'),
+('53', 'NUSA TENGGARA TIMUR'),
+('61', 'KALIMANTAN BARAT'),
+('62', 'KALIMANTAN TENGAH'),
+('63', 'KALIMANTAN SELATAN'),
+('64', 'KALIMANTAN TIMUR'),
+('65', 'KALIMANTAN UTARA'),
+('71', 'SULAWESI UTARA'),
+('72', 'SULAWESI TENGAH'),
+('73', 'SULAWESI SELATAN'),
+('74', 'SULAWESI TENGGARA'),
+('75', 'GORONTALO'),
+('76', 'SULAWESI BARAT'),
+('81', 'MALUKU'),
+('82', 'MALUKU UTARA'),
+('91', 'P A P U A'),
+('92', 'PAPUA BARAT');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `provinsi`
+--
+ALTER TABLE `provinsi`
+  ADD PRIMARY KEY (`kode`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
